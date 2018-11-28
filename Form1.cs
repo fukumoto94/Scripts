40035495b1615cba1239c76daf3e74c7ce2fc52c
Lucas-PC <lucas.fukumoto@hotmail.com>
2018-11-28 12:04:37 -0200

    Scripts

---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsBar
+{
+    public partial class Form1 : Form
+    {
+        //Mouse position
+        private int xPos;
+        private int yPos;
+
+        private Rectangle[] buttonOriginalRect;
+        private Size formOriginalSize;
+        private Button[] bt;
+        private Random rand;
+        private string[] imgTypes = new string[5];
+        private int qdtButton;
+
+        public Form1()
+        { 
+            InitializeComponent();
+        }
+
+        //create Button and add properties
+        private void CreateBt(int qtd)
+        {
+            for (int i = 0; i < qtd; i++)
+            {
+                bt[i] = new Button();
+                bt[i].AllowDrop = true;
+                bt[i].FlatStyle = FlatStyle.Flat;
+                bt[i].BackgroundImageLayout = ImageLayout.Stretch;
+                bt[i].Size = new Size(65,65);
+                bt[i].Name = "";
+                bt[i].Location = new Point(rand.Next(10, (this.Width - (bt[i].Size.Width +10))), rand.Next(10, (this.Height - (bt[i].Size.Height +10))));
+                buttonOriginalRect[i] = new Rectangle(bt[i].Location.X, bt[i].Location.Y, bt[i].Width, bt[i].Height);
+                AddEventBt(bt[i]);
+                this.Controls.Add(bt[i]);
+            }
+        }
+        //add event to button created
+        private void AddEventBt(Button bt)
+        {
+            bt.Click     += btClick;
+            bt.DragEnter += btDragEnter;
+            bt.DragDrop  += btDragDrop;
+            bt.MouseDown += btMouseDown;
+            bt.MouseMove += btMouseMove;
+            bt.MouseUp   += btMouseUp;
+        }
+
+        #region Drag Drop itens
+        private void btDragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void btDragDrop(object sender, DragEventArgs e)
+        {
+            Button bt = sender as Button;
+
+            string[] droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in droppedFiles)
+            {
+                string fileName = getFileName(file);
+                textBox1.Text = file;
+                bt.FlatAppearance.BorderSize = 0;
+                bt.Text = fileName;
+                bt.Name = file;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if(file.Contains(imgTypes[i]))
+                    {
+                       
+                        bt.BackgroundImage = Image.FromFile(file);
+                        break;
+                    }
+                    else if(i == 4)
+                    {
+                        if(file.Contains("."))
+                        {
+                            Icon IEIcon = Icon.ExtractAssociatedIcon(file);
+                            Image im = IEIcon.ToBitmap();
+                            bt.BackgroundImage = im;
+                        }
+                        else
+                        {
+                            bt.BackgroundImage = Image.FromFile(Directory.GetParent(@"..\..\..\").FullName + "\\folderIcon.png");
+
+                        }
+
+                    }
+                }
+               
+
+            }
+        }
+        #endregion
+
+        //Get filename
+        private string getFileName(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
+        }
+
+        #region Mouse events
+        private void btClick(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            if (bt.Name != "")
+            {
+                Process.Start(bt.Name);
+            }
+        }
+    
+        private void btMouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right)
+            {
+                xPos = e.X;
+                yPos = e.Y;
+            }
+        }
+
+        private void btMouseMove(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b != null)
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    b.Top += (e.Y - yPos);
+                    b.Left += (e.X - xPos);
+                }
+            }
+        }
+        private void btMouseUp(object sender, MouseEventArgs e)
+        {
+            for(int i = 0; i < qdtButton; i++)
+            {
+                buttonOriginalRect[i] = new Rectangle(bt[i].Location.X, bt[i].Location.Y, buttonOriginalRect[i].Width, buttonOriginalRect[i].Height);
+            }
+        }
+        #endregion
+
+        //start initial properties
+        private void Form1_Load(object sender, EventArgs e)
+        {  
+            //img types compatibles
+            imgTypes[0] = ".jpg";
+            imgTypes[1] = ".jpeg";
+            imgTypes[2] = ".png";
+            imgTypes[3] = ".bmp";
+            imgTypes[4] = ".gif";
+
+            formOriginalSize = this.Size;
+        }
+
+        #region Resize itens form 
+        private void resizeChildrenControls()
+        {
+            for (int i = 0; i < qdtButton; i++)
+            {
+                resizeControl(buttonOriginalRect[i], bt[i]);
+
+            }
+        }
+        private void resizeControl(Rectangle originalControlRect, Control control)
+        {
+            float xRatio = (float)(this.Width) / (float)(formOriginalSize.Width);
+            float yRatio = (float)(this.Height) / (float)(formOriginalSize.Height);
+
+            int newX = (int)(originalControlRect.X * xRatio);
+            int newY = (int)(originalControlRect.Y * yRatio);
+            int newWidth = (int)(originalControlRect.Width * xRatio);
+            int newHeight = (int)(originalControlRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
+        }
+      
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            resizeChildrenControls();
+        }
+        #endregion
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                qdtButton = Int32.Parse(textBox1.Text);
+            }
+
+            bt = new Button[qdtButton];
+            rand = new Random(qdtButton);
+            buttonOriginalRect = new Rectangle[qdtButton];
+            CreateBt(qdtButton);
+
+        }
+    }
+}
---
+++ PlayerAttack.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : PlayerAnimation
+{
+    //itens
+    public GameObject[] weapon;
+
+    //particles
+    [Header ("Particulas")]
+    public GameObject[] particle;
+
+
+    [Header("SkillsPrefabs")]
+    public GameObject powerPrefab;
+    public GameObject earthShatterPrefab;
+
+    [HideInInspector]
+    public bool canAttack;
+
+    private bool nextAttack = true;
+    private bool rangeAttack = true;
+
+    public AvatarMask[] attackMask;
+
+    private void Awake()
+    {
+        canAttack = true;
+    }
+
+    void Update ()
+    {
+        Debug.Log(canAttack);
+
+        if (canAttack)
+        {
+            InputAttacks();
+            ParticleSystem();
+            ActivateWeapon();
+        }
+        
+    }
+
+
+    private void InputAttacks()
+    {     
+        Attack(1, Input.GetButton("Fire1") && nextAttack);
+        Attack(2, Input.GetButton("Fire2") && nextAttack);
+
+        Skill(1, Input.GetKey(KeyCode.Q) && nextAttack);
+        Skill(2, Input.GetKey(KeyCode.E) && nextAttack);
+        Skill(3, Input.GetKey(KeyCode.R) && nextAttack);
+
+        if (Input.GetButton("Fire1") && nextAttack)
+        {
+    
+            StartCoroutine(OnCompleteAttackAnimation(0.5f));
+        }
+        else if(Input.GetButton("Fire2") && nextAttack)
+        {
+            StartCoroutine(OnCompleteAttackAnimation(0.5f));
+        }
+        else if (Input.GetKey(KeyCode.Q) && nextAttack)
+        {
+            rangeAttack = true;
+            StartCoroutine(OnCompleteAttackAnimation(0.7f));
+        }
+        else if (Input.GetKey(KeyCode.E) && nextAttack)
+        {
+            rangeAttack = true;
+            StartCoroutine(OnCompleteAttackAnimation(0.8f));
+        }
+        else if (Input.GetKey(KeyCode.R) && nextAttack)
+        {
+            StartCoroutine(OnCompleteAttackAnimation(0.5f));
+        }
+
+    }
+
+    private void ParticleSystem()
+    {     
+        particle[0].SetActive(isAttacking(1));
+        particle[1].SetActive(isAttacking(2));
+        particle[2].SetActive(isAttacking(5) && rangeAttack);
+        particle[4].SetActive(isAttacking(7));
+       
+    }
+    private void RangeAttack()
+    {
+        //Create the power from powerprefab
+        var power = (GameObject)Instantiate(
+            powerPrefab,
+            particle[2].transform.position,
+            particle[2].transform.rotation);
+
+        //add velocity to the power
+        power.GetComponent<Rigidbody>().velocity = this.transform.forward * 10;
+
+        //destroy the bullet after
+          Destroy(power, 1f);
+    }
+    private void MeleeRangeAttack()
+    {
+        //Create the power from powerprefab
+        var power = (GameObject)Instantiate(
+            earthShatterPrefab,
+            particle[3].transform.position,
+            particle[3].transform.rotation);
+
+        //add velocity to the power
+        //power.GetComponent<Rigidbody>().velocity = this.transform.forward * 10;
+
+        //destroy the bullet after
+        Destroy(power, 1.5f);
+    }
+
+    private void ActivateWeapon()
+    {
+        weapon[0].SetActive(!isAttacking(0) && !isAttacking(5));
+        weapon[1].SetActive(isAttacking(0) || isAttacking(5));
+
+    }
+    IEnumerator Rotate(float duration, bool isRotate)
+    {
+        if(isRotate)
+        {
+            Quaternion startRot = transform.rotation;
+            float t = 0.0f;
+            while (t < duration)
+            {
+                t += Time.deltaTime;
+                transform.rotation = startRot * Quaternion.AngleAxis(t / duration * 360f, transform.up); //or transform.right if you want it to be locally based
+                yield return null;
+            }
+            transform.rotation = startRot;
+        }     
+    }
+
+    IEnumerator OnCompleteAttackAnimation(float coldown)
+    {
+        nextAttack = false;
+        yield return new WaitForSeconds(coldown);
+        if(isAttacking(5) && rangeAttack)
+        {
+            RangeAttack();
+            rangeAttack = false;
+        }
+        if (isAttacking(6) && rangeAttack)
+        {
+            MeleeRangeAttack();
+            rangeAttack = false;
+        }
+
+        nextAttack = true;
+    }
+}
